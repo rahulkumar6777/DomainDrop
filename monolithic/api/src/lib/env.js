b13f30fd7edfe72ad18069c50ce44ecab0857283
DeployHub Builder <rahulk48546@gmail.com>
2026-08-15 01:04:09 +0530

    add additional required environment variables for configuration

--- monolithic/api/src/lib/env.js
+++ monolithic/api/src/lib/env.js
@@ -3,7 +3,10 @@ dotenv.config();
 
 const requiredEnvs = [
     "NODE_ENV", "PORT",
+    "ADMIN_EMAIL", "ADMIN_USERNAME", "ADMIN_PASSWORD",
     "MONGODB_URI", "REDIS_URI",
+    "EMAIL_HOST", "EMAIL_PORT", "EMAIL_USER", "EMAIL_PASS",
+    "REFRESH_TOKEN_SECRET", "REFRESH_TOKEN_EXPIRATION", "ACCESS_TOKEN_SECRET", "ACCESS_TOKEN_EXPIRATION",
 ]
 
 for (const env of requiredEnvs) {
@@ -15,6 +18,17 @@ for (const env of requiredEnvs) {
 export const envs = {
     NODE_ENV: process.env.NODE_ENV,
     PORT: process.env.PORT,
+    ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+    ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+    ADMIN_USERNAME: process.env.ADMIN_USERNAME,
     MONGODB_URI: process.env.MONGODB_URI,
     REDIS_URI: process.env.REDIS_URI,
+    EMAIL_HOST: process.env.EMAIL_HOST,
+    EMAIL_PORT: process.env.EMAIL_PORT,
+    EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_PASS: process.env.EMAIL_PASS,
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+    REFRESH_TOKEN_EXPIRATION: process.env.REFRESH_TOKEN_EXPIRATION,
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+    ACCESS_TOKEN_EXPIRATION: process.env.ACCESS_TOKEN_EXPIRATION,
 }
