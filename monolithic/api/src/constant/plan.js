7b4f619a6d6bb2c287015f5b37eae9f35e4ea2c0
DeployHub Builder <rahulk48546@gmail.com>
2026-08-15 00:43:38 +0530

    add plan constants for free and starter subscription tiers

---
+++ monolithic/api/src/constant/plan.js
@@ -0,0 +1,16 @@
+const free = {
+    maxStorage: 1073741824, // 1GB
+    maxFileSize: 209715200, // 200mb
+    maxFiles: 200
+}
+
+const starter = {
+    maxStorage: 5368709120, // 5GB
+    maxFileSize: 1073741824, // 1GB
+    maxFiles: 1000
+}
+
+const plans = {
+    free,
+    starter
+}
