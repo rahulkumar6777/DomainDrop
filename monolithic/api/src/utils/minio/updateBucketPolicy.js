bf138e7edfc504b605ac0d46dddd367c6caa2261
DeployHub Builder <rahulk48546@gmail.com>
2026-08-15 15:29:28 +0530

    fadd updateBucketPolicy function for managing bucket policies

---
+++ monolithic/api/src/utils/minio/updateBucketPolicy.js
@@ -0,0 +1,23 @@
+import { client } from "./minio.js"
+import { buildBucketPolicy } from "./policy.js"
+
+export async function updateBucketPolicy(bucketName, policyType) {
+  const exists = await client.bucketExists(bucketName)
+
+  if (!exists) {
+    throw new Error(`Bucket "${bucketName}" does not exist`)
+  }
+
+  const policy = buildBucketPolicy(bucketName, policyType)
+
+  await client.setBucketPolicy(bucketName, policy)
+
+  console.log(
+    `[minio] "${bucketName}" policy changed to "${policyType}"`
+  )
+
+  return {
+    bucketName,
+    policyType,
+  }
+}
