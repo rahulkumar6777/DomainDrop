26435cfcfbb6c9eb2dda5371afd50300228d37f6
DeployHub Builder <rahulk48546@gmail.com>
2026-08-18 01:29:25 +0530

    add express router and authentication routes

---
+++ monolithic/api/src/routes/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import authRoutes from '../modules/identity/routes/v1/identity.routes.js';
+
+
+const router = express.Router();
+
+
+router.use('/v1/auth', authRoutes)
