0c8751db6103d62649e446ed06e2afba92bb02f7
DeployHub Builder <rahulk48546@gmail.com>
2026-08-18 01:24:11 +0530

    make registration controller

---
+++ monolithic/api/src/modules/identity/controllers/register.controller.js
@@ -0,0 +1,50 @@
+import { validationResult } from "express-validator";
+import { sendError } from '../../../utils/errors/sendError.js'
+import { initRegisterService, verifyRegisterService } from "../services/Register.service.js";
+import { envs } from "../../../lib/env.js";
+
+
+export const initRegisterController = async (req, res) => {
+    try {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                message: errors.array()[0].msg
+            })
+        }
+
+        await initRegisterService(req);
+
+
+        return res.status(200).json({
+            success: true,
+            message: "Otp sent Successfully"
+        });
+
+    } catch (error) {
+        returnError(res, error)
+    }
+}
+
+export const verifyRegisterController = async (req, res) => {
+    try {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                message: errors.array()[0].msg
+            })
+        }
+
+        await verifyRegisterService(req);
+
+        return res.status(200).json({
+            success: true,
+            message: "Registration Verified SuccessFully"
+        })
+
+    } catch (error) {
+        returnError(res, error)
+    }
+}
