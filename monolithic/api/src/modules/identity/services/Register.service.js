8af306cb1a1bdb5ccffb6694b5a37fd61cc9b95b
DeployHub Builder <rahulk48546@gmail.com>
2026-08-18 01:23:56 +0530

    make registration service and validate

---
+++ monolithic/api/src/modules/identity/services/Register.service.js
@@ -0,0 +1,96 @@
+import { getRedisClient } from '../../../config/redis/redis.js';
+import { cacheKey } from '../../../utils/cache/cacheKey.js';
+import { AppError } from '../../../utils/errors/AppError.js';
+import { transporter } from '../../../utils/mail/transporter.js';
+import { User } from '../models/user.model.js';
+
+
+export const initRegisterService= async (req) => {
+
+    const redis = getRedis();
+
+    const { fullName, email, phoneno, password, age } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+
+        if (existingUser.status === 'active') {
+            throw new AppError('user with this email already exist', 409);
+        }
+
+
+        if (existingUser.status === 'pending') {
+            throw new AppError('Registration Already initiated', 400)
+        }
+
+    }
+
+    const REGISTRATION_EXPIRY_MS = 10 * 60 * 1000;
+    const getRegistrationExpiry = () => new Date(Date.now() + REGISTRATION_EXPIRY_MS);
+    const otp = Math.floor(1000 + Math.random() * 9000).toString()
+
+    const newUser = new User({
+        fullname: fullName,
+        email: email,
+        password: password,
+
+    });
+
+    const redisKey = cacheKey.UserOtp(newUser.email);
+    redis.hset(redisKey, {
+        email: newUser.email,
+        otp: otp
+    });
+    redis.expire(redisKey, 600);
+
+    await transporter.sendMail({
+        from: `"DomainDrop" <${envs.EMAIL_USER}>`,
+        to: newUser.email,
+        subject: 'Your Registration Otp',
+        text: `Your Registration otp is ${otp}`
+    });
+
+    newUser.registrationExpiresAt = getRegistrationExpiry();
+    await newUser.save({ validateBeforeSave: false });
+
+    return;
+}
+
+export const verifyRegisterService = async (req) => {
+    const redis = getRedis();
+
+    const { otp, email } = req.body;
+
+    const redisKey = cacheKey.UserOtp(email);
+    const cachedOtp = await redis.hgetall(redisKey);
+
+    const user = await User.findOne({ email });
+    console.log(user)
+    if (!user) {
+        throw new AppError('Registration expired or Registration not initiated. Please register again', 400);
+    }
+
+    if (user.status !== 'pending') {
+        throw new AppError('Registration cannot be verified for this account', 403);
+    }
+
+    if (user.registrationExpiresAt && user.registrationExpiresAt <= new Date()) {
+        await User.deleteOne({ _id: user._id, status: "pending" });
+        throw new AppError('Registration expired. Please register again', 400);
+    }
+
+    if (cachedOtp.otp !== otp) {
+        throw new AppError("Invalid or Expired Otp", 400);
+    }
+
+    const userData = await User.findOneAndUpdate(
+        { _id: user._id, status: "pending" },
+        {
+            $set: { status: "active" },
+            $unset: { registrationExpiresAt: "" }
+        },
+        { returnDocument: "after" }
+    );
+
+    await redis.del(redisKey);
+}
---
+++ monolithic/api/src/modules/identity/validators/register.validator.js
@@ -0,0 +1,48 @@
+import { body } from 'express-validator';
+
+export const initRegisterValidator = [
+    body('fullName')
+        .trim()
+        .notEmpty()
+        .withMessage('fullname is required')
+        .isString()
+        .withMessage('fullName must be a string')
+        .isLength({ min: 3, max: 30 })
+        .withMessage("fullname length between 3 to 30"),
+    body('email')
+        .trim()
+        .notEmpty()
+        .withMessage('email is required')
+        .isEmail()
+        .normalizeEmail()
+        .withMessage('Invalid Email formet'),
+    body('password')
+        .notEmpty()
+        .withMessage('password is required')
+        .isString()
+        .withMessage("password must be a String")
+        .isLength({ min: 8, max: 30 })
+        .withMessage("password length between 6 to 30")
+        .matches(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+        )
+        .withMessage(
+            "Password must be characters and include uppercase, lowercase, number, and special character"
+        )
+]
+
+export const verifyRegisterValidator = [
+    body("otp")
+        .trim()
+        .notEmpty()
+        .withMessage("otp is required")
+        .isLength({ min: 6, max: 6 })
+        .withMessage("otp must be six digit"),
+    body('email')
+        .trim()
+        .notEmpty()
+        .withMessage('email is required')
+        .isEmail()
+        .normalizeEmail()
+        .withMessage('Invalid Email formet'),
+]
