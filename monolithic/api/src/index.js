ffc1c088bde6dcef2ff2c773a30485d052ae3f12
DeployHub Builder <rahulk48546@gmail.com>
2026-08-18 23:06:10 +0530

    feat: set up Express server with middleware and health route

---
+++ monolithic/api/src/index.js
@@ -0,0 +1,25 @@
+import { uptime } from "process";
+import { connectDb, disconnectDb } from "./config/db/mongodb.js";
+import { connectRedis } from "./config/redis/redis.js";
+import app from "./server.js";
+import { envs } from "./lib/env.js";
+
+
+//health route
+app.get('/health', (_req, res) => {
+    return res.status(200).json({
+        success: true,
+        time: new Date().toLocaleTimeString()
+    })
+})
+
+// routers
+import router from "./router/index.js";
+app.use('/api', router)
+
+
+app.listen(envs.PORT, async () => {
+    await connectDb();
+    await connectRedis();
+    console.log(`Server is running on port ${envs.PORT} in ${envs.NODE_ENV} mode`);
+});
---
+++ monolithic/api/src/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+
+const app = express();
+
+
+app.set('trust proxy', true);
+
+
+// middleware to parse JSON request bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// cors
+import cors from 'cors';
+import { corsOption } from "./utils/security/cors.js";
+app.use(cors(corsOption));
+
+
+// cookie-parser
+import cookieParser from 'cookie-parser';
+app.use(cookieParser());
+
+
+// important security middleware
+import helmet from "helmet";
+import hpp from "hpp";
+app.use(helmet());
+app.use(hpp());
+
+export default app;
