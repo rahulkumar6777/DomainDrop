af137415b67ce998506a833ba94aa03065bd176c
DeployHub Builder <rahulk48546@gmail.com>
2026-08-18 22:40:02 +0530

    feat: add login and refresh token routes to identity module

--- monolithic/api/src/modules/identity/routes/v1/identity.routes.js
+++ monolithic/api/src/modules/identity/routes/v1/identity.routes.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { initRegisterValidator, verifyRegisterValidator } from '../../validators/register.validator.js';
 import { initRegisterController, verifyRegisterController } from '../../controllers/register.controller.js';
+import { loginValidator } from '../../validators/login.validator.js';
+import { loginController } from '../../controllers/login.js';
+import { refreshTokenController } from '../../controllers/refreshToken.controller.js';
 
 
 const router = express.Router();
@@ -9,6 +12,14 @@ const router = express.Router();
 // register
 router.post('/register/init', initRegisterValidator, initRegisterController);
 router.post('/register/verify', verifyRegisterValidator, verifyRegisterController);
+
+
+// login
+router.post('/login', loginValidator, loginController);
+
+
+// refresh token
+router.get('/refresh-token', refreshTokenController);
 
 
 export default router;
