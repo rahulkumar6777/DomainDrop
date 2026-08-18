f1e28a394f10eb6a9ab5e3bcd251d7f14ec6702a
DeployHub Builder <rahulk48546@gmail.com>
2026-08-18 22:32:00 +0530

    feat: implement login controller, service, and validation for user authentication

---
+++ monolithic/api/src/modules/identity/services/login.service.js
@@ -0,0 +1,26 @@
+import { AppError } from "../../../utils/errors/AppError.js";
+import { User } from "../models/user.Model.js";
+
+
+export const loginService = async (req) => {
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new AppError('Invalid Credentials', 404)
+    }
+
+    if (user.status !== 'active') {
+        throw new AppError('Your Id is not Active', 403)
+    }
+
+    const verifyPassword = await user.checkpassword(password);
+
+    if (!verifyPassword) {
+        throw new AppError('Invalid Credentials', 401);
+    };
+
+    return user;
+
+}
